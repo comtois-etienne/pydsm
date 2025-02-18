821d30a9ac30b1c7099ad89b01d23705625ef52f
comtois-etienne <comtois.etienne@outlook.com>
2025-02-18 12:12:05 -0500

    Create pipeline.sh

---
+++ pipeline.sh
@@ -0,0 +1,10 @@
+
+./main.py --time reproject data/strath/dsm.tif --save-path data/strath/2950/dsm.tif --epsg 2950
+./main.py --time reproject data/strath/dtm.tif --save-path data/strath/2950/dtm.tif --epsg 2950
+./main.py --time reproject data/strath/orthophoto.tif --save-path data/strath/2950/orthophoto.tif --epsg 2950
+
+./main.py --time ndsm data/strath/2950/dsm.tif data/strath/2950/dtm.tif data/strath/2950/ndsm.tif
+
+./main.py --time crop data/strath/2950/orthophoto.tif data/strath/square.shp --save-path data/strath/2950/ortho_croped.tif
+
+./main.py --time zones data/strath/2950/orthophoto.tif data/strath/2950/ndsm.tif --save-directory processed
