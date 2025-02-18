b368d372f98231682bfd4e1d8ce1fd7d4b967939
comtois-etienne <comtois.etienne@outlook.com>
2025-02-18 15:57:40 -0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,7 @@
 ./main.py --time reproject data/strath/dtm.tif --save-path data/strath/2950/dtm.tif --epsg 2950
 ./main.py --time reproject data/strath/orthophoto.tif --save-path data/strath/2950/orthophoto.tif --epsg 2950
 
-./main.py --time ndsm data/strath/2950/dsm.tif data/strath/2950/dtm.tif data/strath/2950/ndsm.tif
+./main.py --time ndsm data/strath/2950/dsm.tif data/strath/2950/dtm.tif data/strath/2950/ndsm.tif --correct-dtm
 
 ./main.py --time crop data/strath/2950/orthophoto.tif data/strath/square.shp --save-path data/strath/2950/ortho_croped.tif
 
