9a778423a406bdf383a82e4bd28c83bdfa4f5ebb
comtois-etienne <comtois.etienne@outlook.com>
2025-04-11 11:24:06 -0400

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,15 @@
 
-./main.py --time reproject data/strath/dsm.tif --save-path data/strath/2950/dsm.tif --epsg 2950
-./main.py --time reproject data/strath/dtm.tif --save-path data/strath/2950/dtm.tif --epsg 2950
-./main.py --time reproject data/strath/orthophoto.tif --save-path data/strath/2950/orthophoto.tif --epsg 2950
+# 1. reproject geotiff to your local projection
+./main.py reproject dsm.tif --save-path 2950/dsm.tif --epsg 2950
 
-./main.py --time ndsm data/strath/2950/dsm.tif data/strath/2950/dtm.tif data/strath/2950/ndsm.tif --correct-dtm
+# 2. create ndsm from dsm and dtm
+./main.py ndsm dsm.tif dtm.tif --correct-dtm --resize orthophoto.tif
 
-./main.py --time crop data/strath/2950/orthophoto.tif data/strath/square.shp --save-path data/strath/2950/ortho_croped.tif
+# 3.
+./main.py zones orthophoto.tif dsm.tif dtm.tif ndsm.tif --save-directory processed/ --translate-file project/2950/translate.csv --geotiff-base-path project/2950/
 
-./main.py --time zones data/strath/2950/orthophoto.tif data/strath/2950/ndsm.tif --save-directory processed
+# 3. crop the original geoTiff from a shapefile (the shapefile can be created from a csv file)
+./main.py crop orthophoto.tif square.shp --save-path ortho_croped.tif
+
+
+./main.py zones data/evelyn_caisse/2950/orthophoto.tif data/evelyn_caisse/2950/ndsm.tif data/evelyn_caisse/2950/dsm.tif data/evelyn_caisse/2950/dtm.tif --save-directory processed/ --translate-file data/evelyn_caisse/2950/translate.csv 
